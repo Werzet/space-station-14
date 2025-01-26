838b7c0b45523686289e420fb9277a0108aecf52
werzet <10843461+Werzet@users.noreply.github.com>
2025-01-26 11:51:34 +1000

    add bypass link check

--- Content.Client/SS220/Discord/DiscordLinkRequiredState.cs
+++ Content.Client/SS220/Discord/DiscordLinkRequiredState.cs
@@ -1,5 +1,6 @@
 // © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
 using System.Threading;
+using Content.Client.Atmos.Overlays;
 using Content.Client.SS220.UserInterface.DiscordLink;
 using Content.Shared.SS220.Discord;
 using Robust.Client.State;
